525b906b50be0f83f415d6754260e831b9e9c234
jerome <micl.jerome@gmail.com>
2022-05-05 18:07:45 +0200

    création d'une Query personnalisée dans TaskRepository

--- src/main/java/fr/groupeA/famillyBoard/repositories/TaskRepository.java
+++ src/main/java/fr/groupeA/famillyBoard/repositories/TaskRepository.java
@@ -2,8 +2,16 @@ package fr.groupeA.famillyBoard.repositories;
 
 import fr.groupeA.famillyBoard.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT t FROM Task t WHERE t.title = ?1 AND  familyMember.id = ?1")
+    public List<Task> findAllTasksByFamilyMember(String title, ) ;
+
+
 }
