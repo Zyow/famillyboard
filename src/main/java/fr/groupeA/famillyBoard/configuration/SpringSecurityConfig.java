b3bdb9d5baf2608e5c62c586d587b1aff75a26ed
Zyow <Szczypamaxime@gmail.com>
2022-06-01 11:53:15 +0200

    Desactivation de l'auth + ajout config CORS

--- src/main/java/fr/groupeA/famillyBoard/configuration/SpringSecurityConfig.java
+++ src/main/java/fr/groupeA/famillyBoard/configuration/SpringSecurityConfig.java
@@ -1,40 +1,40 @@
-package fr.groupeA.famillyBoard.configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("User").password(passwordEncoder().encode("User123")).roles("USER")
-                .and()
-                .withUser("Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN","USER");
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package fr.groupeA.famillyBoard.configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled=true)
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("User").password(passwordEncoder().encode("User123")).roles("USER")
+//                .and()
+//                .withUser("Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN","USER");
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/admin").hasRole("ADMIN")
+//                .antMatchers("/user").hasRole("USER")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
--- src/main/java/fr/groupeA/famillyBoard/controllers/LoginController.java
+++ src/main/java/fr/groupeA/famillyBoard/controllers/LoginController.java
@@ -1,35 +1,35 @@
-package fr.groupeA.famillyBoard.controllers;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.security.RolesAllowed;
-
-@RestController
-@RequestMapping(path = "api/login")
-public class LoginController {
-
-    @CrossOrigin(origins = "/*")
-    @GetMapping
-    public String login() {
-        return "home";
-    }
-
-    @CrossOrigin(origins = "/*")
-    @RequestMapping("/user")
-    @RolesAllowed("USER")
-    public String getUser() {
-        return "Welcome, User";
-    }
-
-    @CrossOrigin(origins = "/*")
-    @RequestMapping("/admin")
-    @RolesAllowed("ADMIN")
-    public String getAdmin() {
-        return "Welcome, Admin";
-    }
-
-
-}
+//package fr.groupeA.famillyBoard.controllers;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.annotation.security.RolesAllowed;
+//
+//@RestController
+//@RequestMapping(path = "api/login")
+//public class LoginController {
+//
+//    @CrossOrigin(origins = "/*")
+//    @GetMapping
+//    public String login() {
+//        return "home";
+//    }
+//
+//    @CrossOrigin(origins = "/*")
+//    @RequestMapping("/user")
+//    @RolesAllowed("USER")
+//    public String getUser() {
+//        return "Welcome, User";
+//    }
+//
+//    @CrossOrigin(origins = "/*")
+//    @RequestMapping("/admin")
+//    @RolesAllowed("ADMIN")
+//    public String getAdmin() {
+//        return "Welcome, Admin";
+//    }
+//
+//
+//}
