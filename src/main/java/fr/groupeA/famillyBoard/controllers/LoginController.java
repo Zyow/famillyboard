91a7a62f4aef153542527922fa74d51675064f14
Vincent80800 <vincent.boulard@yahoo.fr>
2022-06-01 10:05:01 +0200

    ajout de spring security

---
+++ src/main/java/fr/groupeA/famillyBoard/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package fr.groupeA.famillyBoard.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+public class LoginController {
+
+    @GetMapping
+    public String login() {
+        return "home";
+    }
+
+    @RequestMapping("/*")
+    @RolesAllowed("USER")
+    public String getUser() {
+        return "Welcome, User";
+    }
+
+    @RequestMapping("/admin")
+    @RolesAllowed("ADMIN")
+    public String getAdmin() {
+        return "Welcome, Admin";
+    }
+
+
+}
---
+++ src/main/java/fr/groupeA/famillyBoard/controllers/SignInController.java
@@ -0,0 +1,34 @@
+package fr.groupeA.famillyBoard.controllers;
+import fr.groupeA.famillyBoard.entities.User;
+import fr.groupeA.famillyBoard.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value= "/*")
+public class SignInController {
+
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String signIn() {
+        return "*";
+    }
+
+    @PostMapping
+    public String signIn(@Valid User user, HttpServletRequest http) throws ServletException {
+
+        String mdp = user.getPassword();
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        this.userRepository.save(user);
+        http.login(user.getEmail(), mdp);
+        return "*";
+    }
+}
