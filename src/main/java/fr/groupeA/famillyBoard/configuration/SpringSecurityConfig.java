8c312505796d491b5ea9130d768fca4662a20fb6
Zyow <Szczypamaxime@gmail.com>
2022-06-02 09:53:20 +0200

    Debut du systeme d'authentification back

--- src/main/java/fr/groupeA/famillyBoard/configuration/SpringSecurityConfig.java
+++ src/main/java/fr/groupeA/famillyBoard/configuration/SpringSecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -23,30 +22,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.OPTIONS, "/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated()
-                .and()
+                .authenticated().and()
+//                .formLogin().and()
                 .httpBasic();
     }
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("User").password(passwordEncoder().encode("User123")).roles("USER")
-//                .and()
-//                .withUser("Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN","USER");
-//    }
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .antMatchers("/user").hasRole("USER")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 }
